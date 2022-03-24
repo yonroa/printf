3571eeb6dee5b8d14cb4d435f726912b56a53629
yonroa <yonanderson11@gmail.com>
2022-03-23 19:26:49 -0500

    _putchar modified

--- Wri_functions.c
+++ Wri_functions.c
@@ -7,7 +7,20 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	char buf[1024];
+	int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
 
 /**
